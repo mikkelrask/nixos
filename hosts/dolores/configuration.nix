eb6b17af57f0069bfeadc4490cbf462d303fa26a
Mikkel Rask <mikkelrask@pm.me>
2025-02-28 09:57:34 +0100

    0864b2903d4c870d7dd357984b9f0958

--- hosts/dolores/configuration.nix
+++ hosts/dolores/configuration.nix
@@ -26,12 +26,6 @@ in
     loader.efi.canTouchEfiVariables = true;
     plymouth = {
       enable = true;
-      theme = "rings";
-      themePackages = with pkgs; [
-        # By default we would install all themes
-        (adi1090x-plymouth-themes.override {
-          selected_themes = [ "rings" ];
-        })
       ];
     };
 
